8398030b7fc0ab222f7f561f1d991ae41bbf4657
Viktoria <v.s.shuvaeva@gmail.com>
2024-10-25 11:59:56 +0700

    fix_openai

--- openai_image_app.py
+++ openai_image_app.py
@@ -11,7 +11,10 @@ import pytesseract
 import json
 import requests
 from utils import convert_image_to_base64
+from prompt import prompt
 import logging
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -19,15 +22,18 @@ load_dotenv()
 
 # Проверка ориентации изображения
 def check_text_orientation(image):
-    # Преобразуем изображение в градации серого
+    logger.debug("Преобразование изображения в градации серого для проверки ориентации текста.")
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     text = pytesseract.image_to_string(gray)
-    # Определяем количество символов. Если их достаточно, считаем изображение правильным.
-    return len(text) > 10
+    logger.debug(f"Извлеченный текст: {text}")
+    result = len(text) > 10
+    logger.info(f"Ориентация текста {'правильная' if result else 'неправильная'} (количество символов: {len(text)}).")
+    return result
 
 # Поворот изображения на 90 градусов
 def rotate_image_90_degrees(image, clockwise=False):
-    # Поворот изображения на 90 градусов
+    direction = "по часовой стрелке" if clockwise else "против часовой стрелки"
+    logger.info(f"Поворот изображения на 90 градусов {direction}.")
     if clockwise:
         return cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
     else:
@@ -36,101 +42,77 @@ def rotate_image_90_degrees(image, clockwise=False):
 # Извлечение номера накладной с помощью openai
 async def get_invoice_from_image(base64_image):
     try:
-        
+        logger.info("Начало запроса к OpenAI для извлечения номера накладной.")
         response = await client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
                 {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": prompt},
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}",
-                    },
-                    },
-                ],
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": prompt},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}",
+                            },
+                        },
+                    ],
                 }
             ],
             max_tokens=300,
         )
-        # Извлечение содержимого content
         content = response.choices[0].message.content
-        logger.info(f"Извлечен номер накладной: {content}.")
+        logger.info(f"Успешно извлечен номер накладной: {content}")
         return content
     except Exception as e:
-        logger.error(f"Произошла общая ошибка: {e}")
+        logger.error(f"Ошибка при запросе к OpenAI: {e}")
         return {"error": str(e)}
 
-openai.api_key=os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 client = AsyncOpenAI()
-
-prompt="""
-{
-    "role": "Ты ассистент логиста транспортной компании, ты интегрирован в чат telegram. ТЫ получаешь фотографии из чата",
-    "prompt": "Ты можешь получать различные фотографии, в том числе фотографии документов на перевозку груза, или любые другие фотографии, например фотографии грузов",
-    "tasks": [
-        {
-            "task01": {
-                "task": "определи, предоставлена ли тебе фотография документа на перевозку груза, содержащая данные отправителя, получателя и номер документа",
-                "result01": "Если фотография является документом на перевозку груза переходи к task02",
-                "result02": "Если фотография не является документом на перевозку груза запиши в переменную error значение True, в переменную number значение "Номер накладной отсутствует""
-            }
-        },
-        {
-            "task02": "Если фотография является документом на перевозку груза, найди на фотографии номер документа.  Это самая крупная надпись печатными цифрами в левом или правом верхнем углу. Найди на фотографии номер документа. Все, что после номера игнорируй (например, если написано АЗ-22-04 от 29.12.2022, то номер это  АЗ-22-04). запиши в переменную error значение False,  в переменную number Номер документа"
-        },
-        {
-            "task03": "верни в ответе объект с ключами и значениями error и number, например ",
-            "example01": {
-                "number": "АЗ-22-04",
-                "error": "False"
-            },
-            "example02": {
-                "number": "Номер накладной отсутствует",
-                "error": "True"
-            }
-        }
-    ]
-}
-"""
 
 # Асинхронная функция для обработки изображения и извлечения номера накладной
 async def get_number_using_openai(base64_image):
     try:
-        # Декодируем изображение из base64
+        logger.info("Начало обработки изображения для извлечения номера накладной.")
         image_data = base64.b64decode(base64_image)
+        logger.debug("Изображение успешно декодировано из base64.")
+        
         try:
-            # Создаем поток для работы с изображением
+            logger.debug("Создание потока для работы с изображением.")
             image_stream = io.BytesIO(image_data)
             pil_image = Image.open(image_stream)
+            logger.info("Изображение загружено в PIL.")
             
-            # Конвертируем изображение в формат RGB
             pil_image = pil_image.convert("RGB")
+            logger.debug("Изображение сконвертировано в формат RGB.")
             cv_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
-            
-            # Проверка ориентации текста
+            logger.info("Изображение преобразовано в формат OpenCV для дальнейшей обработки.")
+
             if not check_text_orientation(cv_image):
-                # Поворот изображения, если текст не читается
-                for _ in range(3):
+                logger.info("Текст не читается. Поворот изображения.")
+                for attempt in range(3):
                     cv_image = rotate_image_90_degrees(cv_image, clockwise=False)
                     if check_text_orientation(cv_image):
+                        logger.info(f"Текст стал читаемым после поворота (попытка {attempt + 1}).")
                         break
+                else:
+                    logger.warning("Не удалось сделать текст читаемым после трех поворотов.")
 
-            # Конвертируем изображение обратно в base64
             base64_image = convert_image_to_base64(cv_image)
+            logger.debug("Изображение перекодировано обратно в base64 для отправки в OpenAI.")
             
-            # Асинхронно извлекаем данные накладной
             invoice_data = await get_invoice_from_image(base64_image)
+            logger.info("Номер накладной успешно извлечен.")
             return json.loads(invoice_data)
         finally:
-            # Закрываем изображения и поток
             pil_image.close()
             image_stream.close()
+            logger.debug("Поток и изображение закрыты.")
     except requests.RequestException as e:
+        logger.error(f"Ошибка сети при запросе: {e}")
         return {"error": str(e)}
     finally:
         del image_data, image_stream, pil_image, cv_image
-
+        logger.debug("Очищены временные данные для освобождения памяти.")
     
