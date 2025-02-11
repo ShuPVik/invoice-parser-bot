dc16bc26e7779538d43f73357939c46c809312ba
Viktoia Polikanova <v.s.polikanova@gmail.com>
2025-02-11 18:09:02 +0700

    logging on start

--- logger.py
+++ logger.py
@@ -8,14 +8,13 @@ if not os.path.exists('logs'):
 # Настроим логирование
 logging.basicConfig(
     level=logging.INFO,  # Уровень логов
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Формат логов
+    # Формат логов
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('logs/bot.log', mode='a', encoding='utf-8'),  # Запись логов в файл
+        logging.FileHandler('logs/bot.log', mode='a',
+                            encoding='utf-8'),  # Запись логов в файл
         logging.StreamHandler()  # Дублирование логов в консоль
     ]
 )
 
 logger = logging.getLogger(__name__)
-
-# Пример использования логгера
-logger.info("Бот запущен!")
--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 from handlers import router  # Импортируем router из handlers
 from utils import get_main_keyboard
-
-
+from logger import logger
 # Загрузить переменные окружения
 load_dotenv()
 API_TOKEN = os.getenv('TG_API_TOKEN')
@@ -20,10 +19,12 @@ dp.include_router(router)
 
 async def on_startup(dispatcher: Dispatcher):
     """Функция запуска бота, устанавливает клавиатуру по умолчанию"""
+    logger.info("Клаиватура установлена")
     dispatcher["default_reply_markup"] = get_main_keyboard()
 
 
 async def main():
+    logger.info('Бот запущен')
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
